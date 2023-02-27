a70f71d56c14f2357e0279f5096015f5e28859a9
• Kŕrish <83119148+HKrrish@users.noreply.github.com>
2023-02-27 09:17:54 +0530

    Create txt.py

---
+++ helper/txt.py
@@ -0,0 +1,19 @@
+class mr(object):
+    PROGRESS_BAR = """\n
+╭━━━━❰• **Process** •❱━➣
+┣⪼ 🗂️ : {1} | {2}
+┣⪼ ⏳️ : {0}%
+┣⪼ 🚀 : {3}/s
+┣⪼ ⏱️ : {4}
+╰━━━━━━━━━━━━━━━➣ """
+
+    DMCA_TXT = """
+╭───────────⍟
+├**My Name** : {}
+├**Developed By** : (@The_Entertainment)           
+╰───────────────⍟
+This Bot just a archive of movies. We don't Own any content of bot.
+All copyright to their respective owners. The content in bot are taken from different places.
+**Thank you!**
+"""
+    HELP_TXT = """You can use me for searching Old/New movies. You can use me anywhere in telegram. Just type my Username and type Movie Name to Search.\n\n**Example :** `@EMoviezBot Kantara.2022`\n\n**Channel** : (@The_Entertainment) ✨"""
