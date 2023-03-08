3a108c39be237a13fc6a0feadd3cee0354adac80
• Kŕrish <83119148+HKrrish@users.noreply.github.com>
2023-03-09 00:15:05 +0530

    Create nyaasi.py

---
+++ plugins/Torrent/nyaasi.py
@@ -0,0 +1,147 @@
+import os
+import aiohttp
+import json
+from pyrogram import Client, filters, emoji
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+m = None
+i = 0
+a = None
+query = None
+
+
+@Client.on_message(filters.command(["ni"]))
+async def find(_, message):
+    global m
+    global i
+    global a
+    global query
+    try:
+        await message.delete()
+    except:
+        pass
+    if len(message.command) < 2:
+        await message.reply_text("Usage: /ni query")
+        return
+    query = message.text.split(None, 1)[1].replace(" ", "%20")
+    m = await message.reply_text("Searching")
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"https://api-tor.herokuapp.com/nyaasi/{query}") \
+                    as resp:
+                a = json.loads(await resp.text())
+    except:
+        await m.edit("Found Nothing.")
+        return
+    result = (
+        f"**Page - {i+1}**\n\n"
+        f"➲Name: {a[i]['Name']}\n"
+        f"➲Uploaded on {a[i]['Date']}\n"
+        f"➲Torrent: {a[i]['TorrentLink']}\n" 
+        f"➲Type: {a[i]['Category']}\n"
+        f"➲Size: {a[i]['Size']}\n"
+        f"➲Seeds: {a[i]['Seeder']} & "
+        f"➲Leeches: {a[i]['Leecher']}\n"
+        f"➲Magnet: `{a[i]['Magnet']}`\n\n\n"
+    )
+    await m.edit(
+        result,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(f"Next",
+                                         callback_data="next"),
+                    InlineKeyboardButton(f"{emoji.CROSS_MARK}",
+                                         callback_data="delete")
+                ]
+            ]
+        ),
+        parse_mode="markdown",
+    )
+
+
+@Client.on_callback_query(filters.regex("next"))
+async def callback_query_next(_, message):
+    global i
+    global m
+    global a
+    global query
+    i += 1
+    result = (
+        f"**Page - {i+1}**\n\n"
+        f"➲Name: {a[i]['Name']}\n"
+        f"➲Uploaded on {a[i]['Date']}\n"
+        f"➲Torrent: {a[i]['TorrentLink']}\n" 
+        f"➲Type: {a[i]['Category']}\n"
+        f"➲Size: {a[i]['Size']}\n"
+        f"➲Seeds: {a[i]['Seeder']} & "
+        f"➲Leeches: {a[i]['Leecher']}\n"
+        f"➲Magnet: `{a[i]['Magnet']}`\n\n\n"
+    )
+    await m.edit(
+        result,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(f"Prev",
+                                         callback_data="previous"),
+                    InlineKeyboardButton(f"{emoji.CROSS_MARK}",
+                                         callback_data="delete"),
+                    InlineKeyboardButton(f"Next",
+                                         callback_data="next")
+                    
+                ]
+            ]
+        ),
+        parse_mode="markdown",
+    )
+
+
+@Client.on_callback_query(filters.regex("previous"))
+async def callback_query_previous(_, message):
+    global i
+    global m
+    global a
+    global query
+    i -= 1
+    result = (
+        f"**Page - {i+1}**\n\n"
+        f"➲Name: {a[i]['Name']}\n"
+        f"➲Uploaded on {a[i]['Date']}\n"
+        f"➲Torrent: {a[i]['TorrentLink']}\n" 
+        f"➲Type: {a[i]['Category']}\n"
+        f"➲Size: {a[i]['Size']}\n"
+        f"➲Seeds: {a[i]['Seeder']} & "
+        f"➲Leeches: {a[i]['Leecher']}\n"
+        f"➲Magnet: `{a[i]['Magnet']}`\n\n\n"
+    )
+    await m.edit(
+        result,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(f"Prev",
+                                         callback_data="previous"),
+                    InlineKeyboardButton(f"{emoji.CROSS_MARK}",
+                                         callback_data="delete"),
+                    InlineKeyboardButton(f"Next",
+                                         callback_data="next")
+                ]
+            ]
+        ),
+        parse_mode="markdown",
+    )
+
+
+@Client.on_callback_query(filters.regex("delete"))
+async def callback_query_delete(_, message):
+    global m
+    global i
+    global a
+    global query
+    await m.delete()
+    m = None
+    i = 0
+    a = None
+    query = None
