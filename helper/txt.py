55a98ece4d66362567099e150e7538ad0319e2f2
• Kŕrish <83119148+HKrrish@users.noreply.github.com>
2023-03-09 03:18:13 +0530

    Update txt.py

--- helper/txt.py
+++ helper/txt.py
@@ -16,4 +16,4 @@ This Bot just a archive of good movies. We don't Own any content of bot.
 All copyright to their respective owners. The content in bot are taken from different places (sites, channels etc).
 **Thank you!**
 """
-    HELP_TXT = """You can use me for searching Old/New good movies. You can use me anywhere in telegram. Just type my Username and type Movie Name to Search.\n\n**Example :** `@EMoviezBot Bigil`\n\n**Channel** : (@The_Entertainment) ✨"""
+    HELP_TXT = """You can use me for searching Old/New good movies. You can use me anywhere in telegram. Just type my Username and type Movie Name to Search.\n\n**Example :** `@EMoviezBot Bigil`\n\nYou can search torrent also by this bot. ✨\n• /torrent : by default from 1337x\n• /torr_pb : from piratebay\n\n**Channel** : (@The_Entertainment) ✨"""
