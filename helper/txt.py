f8b780935c5d635a7c4b28ecae93f5834ff56dcf
• Kŕrish <83119148+HKrrish@users.noreply.github.com>
2023-03-08 22:56:12 +0530

    Update txt.py

--- helper/txt.py
+++ helper/txt.py
@@ -12,8 +12,8 @@ class mr(object):
 ├**My Name** : {}
 ├**Developed By** : (@The_Entertainment)           
 ╰───────────────⍟
-This Bot just a archive of movies. We don't Own any content of bot.
-All copyright to their respective owners. The content in bot are taken from different places.
+This Bot just a archive of good movies. We don't Own any content of bot.
+All copyright to their respective owners. The content in bot are taken from different places (sites, channels etc).
 **Thank you!**
 """
-    HELP_TXT = """You can use me for searching Old/New movies. You can use me anywhere in telegram. Just type my Username and type Movie Name to Search.\n\n**Example :** `@EMoviezBot Kantara.2022`\n\n**Channel** : (@The_Entertainment) ✨"""
+    HELP_TXT = """You can use me for searching Old/New good movies. You can use me anywhere in telegram. Just type my Username and type Movie Name to Search.\n\n**Example :** `@EMoviezBot Bigil`\n\n**Channel** : (@The_Entertainment) ✨"""
