b43d45586c91483a59abd1dbeb4f42bb6d8f0156
• Kŕrish <83119148+HKrrish@users.noreply.github.com>
2023-03-09 00:56:32 +0530

    Update piratebay.py

--- plugins/Torrent/piratebay.py
+++ plugins/Torrent/piratebay.py
@@ -1,7 +1,7 @@
 import os
 import aiohttp
 import json
-from pyrogram import Client, filters, emoji
+from pyrogram import Client, filters, emoji, enums 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 m = None
@@ -11,7 +11,7 @@ query = None
 
 
 @Client.on_message(filters.command(["pb"]))
-async def find(_, message):
+async def findpb(_, message):
     global m
     global i
     global a
@@ -56,7 +56,7 @@ async def find(_, message):
                 ]
             ]
         ),
-        parse_mode="markdown",
+        parse_mode=enums.ParseMode.MARKDOWN,
     )
 
 
@@ -93,7 +93,7 @@ async def callback_query_next(_, message):
                 ]
             ]
         ),
-        parse_mode="markdown",
+        parse_mode=enums.ParseMode.MARKDOWN,
     )
 
 
@@ -129,7 +129,7 @@ async def callback_query_previous(_, message):
                 ]
             ]
         ),
-        parse_mode="markdown",
+        parse_mode=enums.ParseMode.MARKDOWN,
     )
 
 
