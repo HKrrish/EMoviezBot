9f1494dba83265bbc2fb2dae3fce12e97179a327
• Kŕrish <83119148+HKrrish@users.noreply.github.com>
2023-03-09 09:39:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -48,7 +48,7 @@ support_chat_id = environ.get('SUPPORT_CHAT_ID')
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
 INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
 
-IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "<b>🎬 Title:</b> <a href={url}>{title}</a> [{year}] —<b>{kind}</b>\n\n<b>📆 Release:</b> <a href={url}/releaseinfo>{release_date}</a>\n<b>🌟 Rating:</b> <a href={url}/ratings>{rating} / 10</a>\n(based on <code>{votes}</code> user ratings.)\n\n<b>🎭 Genres:</b> #{genres}\n<b>📀 Runtime:</b> <code>{runtime} minutes</code>\n\n<b>☀️ Languages:</b> #{languages}\n<b>🌎 Country of Origin:</b> #{countries}\n<b>🎥 Director:</b> {director}\n\n<b><a href='https://t.me/+q9NMdy0rY10zZWZl'>© IMDb (Series & Movie) Studio</a></b>\n\n<b>✍️ Note:</b> <s>This message will be Auto-deleted after 10 hours to avoid copyright issues.</s>")
+IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "<b>🎬 <u>Title</u> :</b> <a href={url}>{title}</a> ({year}) — <b>{kind}</b>\n\n<b>🎭 Genres </b>: {genres}\n<b>🌟 Rating :</b> <a href={url}/ratings>{rating} / 10</a>\n(based on <code>{votes}</code> user ratings.)\n\n<b>📀 Runtime :</b> <code>{runtime} minutes</code>\n\n<b>☀️ Languages :</b> {languages}\n<b>🌎 Country of Origin:</b> #{countries}\n<b>🎥 Director:</b> {director}\n\n<b>💥 <u>Storyline</u></b> : {plot}\n\n<b>(@The_Entertainment)</b>")
 LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION")
